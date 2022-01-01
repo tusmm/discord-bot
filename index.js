209a24b4e525fe3658e2cf580cbb0f5a1ac86d0f
Ryan O <40081025+tusmm@users.noreply.github.com>
2022-01-01 04:05:22 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, DiscordAPIError, Discord } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const prefix = '!';
+
+const  fs = require('fs');
+
+const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for (const file of commandFiles){
+	const command = require(`./commands/${file}`);
+
+	client.commands.set(command.name, command);
+
+}
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -11,10 +24,13 @@ client.once('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
-	if (message.content === 'ping') {
-		message.reply({
-			content: 'pong',
-		})
+	if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
+		client.commands.get('ping').execute(message, args);
 	}
 })
 
